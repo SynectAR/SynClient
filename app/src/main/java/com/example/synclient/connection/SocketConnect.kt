857a0e549acd8d61c85c4bba5ccaae998261dda2
Nikita <gromovnik99@gmail.com>
2021-11-17 12:16:43 +0700

    Added all files and removed unnecessary ones.

---
+++ app/src/main/java/com/example/synclient/connection/SocketConnect.kt
@@ -0,0 +1,56 @@
+package com.example.synclient.connection
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import java.io.BufferedInputStream
+import java.io.DataInputStream
+import java.net.Socket
+
+public class SocketConnect(address: String, port: Int) {
+    private val socket: Socket = Socket(address,port)
+
+    fun closeSocket():Boolean{
+        socket.close()
+        return true
+    }
+    fun getBitmap(): Bitmap{
+        var bytesArray = getMessageByCustomProtocol(socket)
+        return convertToBitmap(bytesArray)
+    }
+
+    private fun getMessageByCustomProtocol(socket: Socket):ByteArray{
+        val `in` = DataInputStream(BufferedInputStream(socket.getInputStream()))
+        val messageMetadata = ByteArray(4)
+        `in`.read(messageMetadata)
+        val messageSize = convertBytesToInt(messageMetadata)
+        val messageFromServer = ByteArray(messageSize)
+        `in`.read(messageFromServer)
+        `in`.close()
+        return messageFromServer
+    }
+
+    private fun convertToBitmap(bytes: ByteArray): Bitmap {
+        val pictureSize = bytes.size / 4
+        val pixels: Int = kotlin.math.sqrt(pictureSize.toDouble()).toInt()
+        val resultBitmap = Bitmap.createBitmap(pixels, pixels, Bitmap.Config.ARGB_8888)
+        for(j in 0 until pixels)
+        for(i in 0 until pixels){
+            val index = i * pixels + j
+            resultBitmap.setPixel(i,j, Color.rgb(convertByteToInt(bytes,index*4+2),
+                    convertByteToInt(bytes,index*4+1),convertByteToInt(bytes,index*4+3)))
+        }
+        return resultBitmap
+    }
+
+    private fun convertBytesToInt(bytes: ByteArray): Int {
+        return (bytes[3].toInt() shl 24) or
+                (bytes[2].toInt() and 0xff shl 16) or
+                (bytes[1].toInt() and 0xff shl 8) or
+                (bytes[0].toInt() and 0xff)
+    }
+
+    private fun convertByteToInt(bytes: ByteArray, shift: Int): Int {
+        return (bytes[0+shift].toInt() and 0xff)
+    }
+
+}
---
+++ app/src/main/java/com/example/synclient/ui/ARActivity.kt
@@ -0,0 +1,12 @@
+package com.example.synclient.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.synclient.R
+
+class ARActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_ar)
+    }
+}
