2c8d838e1fb789dfb9d27b3974b867f7f75c168b
Nikita <gromovnik99@gmail.com>
2021-12-17 15:57:45 +0700

    Changed MainActivity to display messages from server in Dashboard fragment on buttonClick event.

--- app/src/main/java/com/example/synclient/MainActivity.kt
+++ app/src/main/java/com/example/synclient/MainActivity.kt
@@ -7,22 +7,10 @@ import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.synclient.databinding.ActivityMainBinding
-import com.example.synclient.grpcFlow.GRPCClient
-import io.grpc.ManagedChannelBuilder
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import android.widget.Toast
-import io.grpc.ClientCall
-import org.bouncycastle.its.asn1.EndEntityType
-
-import org.bouncycastle.its.asn1.EndEntityType.app
-
-
+import com.example.synclient.calibrationHelper.CalibrationHelper
+import kotlinx.coroutines.*
 
 
 
@@ -43,36 +31,11 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
-    public fun buttonGRPCClicked(v: View)
-    {
-        var textRe: String = "Не получил."
-        var  responseText: String = "test"
-        val client= createChannelForClient()
-            CoroutineScope(Dispatchers.IO).launch {
-                responseText= client.sayHello("KEKW")
-            }
-        val view= findViewById<Button>(R.id.buttonGRPC)
-        view?.text=responseText
+    public fun buttonGRPCClicked(v: View) {
+        val view = findViewById<Button>(R.id.buttonGRPC)
+        var buttonText: String? = "Не получил"
+        runBlocking { buttonText = CalibrationHelper.sayHello("Получил") }
+        view.text = buttonText
     }
-
-    suspend fun sayHelloFromClient(): String
-    {
-        val client= createChannelForClient()
-        var text= client.sayHello("kekw")
-        return text
-    }
-
-    fun createChannelForClient() : GRPCClient
-    {
-        val port = System.getenv("PORT")?.toInt() ?: 50051
-
-        val channel = ManagedChannelBuilder
-            .forAddress("10.0.2.2", port)
-            .usePlaintext()
-            .build()
-        val client = GRPCClient(channel)
-        return client
-    }
-
 
 }
